7dae1860a4f7e891b98e76108088f09fcde45f78
roma-turkin <roma.turkin@yahoo.com>
2014-10-24 16:02:24 +0400

    It seems to work. Maybe.

--- SimpleApp-war/src/java/com/simpleapp/beans/UserBean.java
+++ SimpleApp-war/src/java/com/simpleapp/beans/UserBean.java
@@ -1,28 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.simpleapp.beans;
 
 import com.simpleapp.user.User;
 import java.io.Serializable;
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.SessionScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
-/**
- *
- * @author snape
- */
-
+@ManagedBean
 @SessionScoped
 public class UserBean implements Serializable {
 
    private User user;
+   private String name;
    
    @PostConstruct
    private void init() {
-       user.initUser("Sergey Shilin");
+       user = new User();
+   }
+   public void setUser(User user) {
+       this.user = user;
+   }
+   public User getUser() {
+       return this.user;
+   }
+   public void setName(String name) {
+       this.name = name;
+   }
+   public String getName() {
+       return this.name;
    }
 }
